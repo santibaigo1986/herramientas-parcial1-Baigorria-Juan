467be67cd7f4cc45cdcb42267e76d4cefb819e82
Baigorria Juan <santibaigo1986@gmail.com>
2023-10-10 18:20:29 -0300

    AfterDB3

--- BaigorriaJuan/Models/Book.cs
+++ BaigorriaJuan/Models/Book.cs
@@ -1,18 +1,35 @@
 ﻿namespace BaigorriaJuan.Models
 {
     public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public int PublicationYear { get; set; }
+    public string Genre { get; set; }
+    public bool IsBestSeller { get; set; }
+    public decimal Price { get; set; }
+    public string Author { get; set; }
+    public string CoverImage { get; set; }
+
+    public int? PublisherId { get; set; }
+    public virtual Publisher Publisher { get; set; }
+
+    public Book()
     {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public int PublicationYear { get; set; }
-        public string Genre { get; set; }
-        public bool IsBestSeller { get; set; }
-        public decimal Price { get; set; }
-        public string Author { get; set; }
-        public string CoverImage { get; set; }
-
-        public int? PublisherId { get; set; }
-
-        public virtual Publisher Publisher { get; set; }
+        // Constructor vacío requerido para Entity Framework
     }
+
+    public Book(string title, int publicationYear, string genre, bool isBestSeller, decimal price, string author, string coverImage, int? publisherId = null)
+    {
+        Title = title;
+        PublicationYear = publicationYear;
+        Genre = genre;
+        IsBestSeller = isBestSeller;
+        Price = price;
+        Author = author;
+        CoverImage = coverImage;
+        PublisherId = publisherId;
+    }
+}
+
 }
--- BaigorriaJuan/Models/Publisher.cs
+++ BaigorriaJuan/Models/Publisher.cs
@@ -1,11 +1,24 @@
 ﻿namespace BaigorriaJuan.Models
 {
     public class Publisher
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    
+    // Navigation property
+    public ICollection<Book> Books { get; set; }
 
-        // Navigation property
-        public ICollection<Book> Books { get; set; }
+    public Publisher()
+    {
+        // Constructor vacío requerido para Entity Framework
+        Books = new List<Book>();
     }
+
+    public Publisher(string name)
+    {
+        Name = name;
+        Books = new List<Book>();
+    }
+}
+
 }
